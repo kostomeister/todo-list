a50e91e829b1f27132097e0c210cac66114c8df2
Kostia <kostia3507@gmail.com>
2023-11-27 12:52:15 +0200

    added form for creating and updating tags

--- todo/forms.py
+++ todo/forms.py
@@ -15,3 +15,9 @@ class TaskForm(forms.ModelForm):
         widgets = {
             'deadline': forms.TextInput(attrs={'type': 'datetime-local'}),
         }
+
+
+class TagForm(forms.ModelForm):
+    class Meta:
+        model = Tag
+        fields = "__all__"

6d47d5372fcb05c52145d178e6baf74e442434a9
Kostia <kostia3507@gmail.com>
2023-11-27 13:00:10 +0200

    added view for tag creating

--- todo/views.py
+++ todo/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import generic
 
-from .forms import TaskForm
+from .forms import TaskForm, TagForm
 from .models import Task, Tag
 
 
@@ -42,3 +42,9 @@ class TagListView(generic.ListView):
 
 class TagDetailView(generic.DetailView):
     model = Tag
+
+
+class TagCreateView(generic.CreateView):
+    model = Tag
+    form_class = TagForm
+    success_url = reverse_lazy("todo:tag-list")

b91e049d0371b38a457ee8e10b2eaf546ec31d27
Kostia <kostia3507@gmail.com>
2023-11-27 12:04:12 +0200

    created view for task details page & added it to urls.py

--- todo/urls.py
+++ todo/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from todo.views import TaskListView
+from todo.views import TaskListView, TaskDetailView
 
 urlpatterns = [
-    path("", TaskListView.as_view(), name="index")
+    path("", TaskListView.as_view(), name="index"),
+    path("/<int:pk>/", TaskDetailView.as_view(), name="task-detail")
 ]
 
 app_name = "todo"
--- todo/views.py
+++ todo/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
 from django.views import generic
-
 from .models import Task
 
 
 class TaskListView(generic.ListView):
     model = Task
     paginate_by = 7
+
+    def get_queryset(self):
+        return Task.objects.prefetch_related("tags")
+
+
+class TaskDetailView(generic.DetailView):
+    model = Task
 
     def get_queryset(self):
         return Task.objects.prefetch_related("tags")
